5a378f38e2bd45aa24b1861736b6c52c85cdc2bf
Harsh Sharma <124427013+harshh-sharma@users.noreply.github.com>
2024-11-27 21:25:19 +0530

    adding whitelist the frontend url

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,16 @@ import cookieParser from 'cookie-parser';
 import express from 'express';
 import userRouter from './routes/user.route.js';
 import taskRouter from './routes/task.route.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors({
+   origin:[process.env.FRONTEND_URL],
+   credentials:true
+}));
 app.use(cookieParser());
 
 app.get('/ping',(req,res) => {
