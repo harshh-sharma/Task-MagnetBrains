af3f6cc3e0108b9c362d3509e3ea6b40e674bdf5
Harsh Sharma <124427013+harshh-sharma@users.noreply.github.com>
2024-11-27 17:47:28 +0530

    adding some middleware and and prepare intial user routing

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
 app.get('/ping',(req,res) => {
    res.send('/pong')
 })
+
+app.use('api/v1/user',userRouter);
+
 
 export default app;
